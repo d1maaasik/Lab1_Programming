0d34f11fdb34719b158cd4b07d8ec3329e903a60
d1maasik <d1maasik05@gmail.com>
2024-03-29 23:59:07 +0700

    second commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
-//nomer 3
-
 #include <iostream>
-// рекурсивная функция для вывода строки n-ое количество раз
+
+// рекурсивная функция для вывода строки n раз
 void printStringsRecursively(int n, const std::string& str, int& count) {
-    if (n <= 0) { // если n меньше или равно 0 раз, то завершаем рекурсию
-        std::cout << "Total function calls: " << count << std::endl; // выводим общее еколимчество вызова функции 
+    // базовый случай: если n меньше или равно 0, завершаем рекурсию
+    if (n <= 0) {
+        std::cout << "Total function calls: " << count << std::endl; // Выводим общее количество вызовов функции
         return;
     }
-
-    std::cout << str << std::endl; 
-    count++;
-    printStringsRecursively(n - 1, str, count); // рекурс вывод строки с меньшим n
+    
+    std::cout << str << std::endl; // выводим строку
+    count++; // увеличиваем счетчик вызовов
+    printStringsRecursively(n - 1, str, count); // Рекурсивный вызов функции с уменьшенным n
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    int n;
-    std::cout << "введите число строк для написания: ";
-    std::cin >> n;
+    int n = 5; // задаем значение переменной n
 
-    int count = 0;
-    printStringsRecursively(n, "хеллоу", count); //выозов рекурс функции
+    int count = 0; // инициализация счетчика вызовов
+    printStringsRecursively(n, "хеллоу", count); // Вызов рекурсивной функции
 
     return 0;
 }
